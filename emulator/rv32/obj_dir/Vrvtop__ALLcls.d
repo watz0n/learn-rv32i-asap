9d53d330133b435a838fbf9ab2edf503c837f3a6
watz0n <watz0n.tw@gmail.com>
2018-01-21 01:44:29 +0800

    Update README.

--- emulator/rv32/obj_dir/Vrvtop__ALLcls.d
+++
@@ -1,9 +0,0 @@
-Vrvtop__ALLcls.o: Vrvtop__ALLcls.cpp Vrvtop.cpp Vrvtop.h \
- /home/wats0n/projects/verilator/include/verilated_heavy.h \
- /home/wats0n/projects/verilator/include/verilated.h \
- /home/wats0n/projects/verilator/include/verilated_config.h \
- /home/wats0n/projects/verilator/include/verilatedos.h Vrvtop__Dpi.h \
- /home/wats0n/projects/verilator/include/vltstd/svdpi.h Vrvtop__Syms.h \
- Vrvtop___024unit.h \
- /home/wats0n/projects/verilator/include/verilated_dpi.h \
- Vrvtop___024unit.cpp
--- emulator/rv32/obj_dir/verilated.d
+++
@@ -1,7 +0,0 @@
-verilated.o: /home/wats0n/projects/verilator/include/verilated.cpp \
- /home/wats0n/projects/verilator/include/verilated_imp.h \
- /home/wats0n/projects/verilator/include/verilatedos.h \
- /home/wats0n/projects/verilator/include/verilated.h \
- /home/wats0n/projects/verilator/include/verilated_config.h \
- /home/wats0n/projects/verilator/include/verilated_heavy.h \
- /home/wats0n/projects/verilator/include/verilated_syms.h
